3a52667d9a18707437d879877b182f7fe9f29666
Oduolperis <oduolpepe618@gmail.com>
2023-08-07 17:14:27 +0300

    created an instance of the housing location interface to src/app/home/home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent }from '../housing-location/housing-location.component'
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -22,4 +23,18 @@ import { HousingLocationComponent }from '../housing-location/housing-location.co
   `,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+
+/* created an instance of the housing location interface*/
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
+}
