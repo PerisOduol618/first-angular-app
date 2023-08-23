6375a93448a9fe5fdee86050edf7e15ff1e1af5d
Oduol Peris <oduolpepe618@gmail.com>
2023-08-23 09:42:43 +0300

    imports directive ,and add form directives

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   template: `
   <article>
     <img class="listing-photo" [src]="housingLocation?.photo"
@@ -34,6 +36,12 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+  
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
